4fec8005e4d2479d10eeab8b0c19fb49497d026b
Jonathan Cohen <cohen_jonathan@bah.com>
2022-07-18 12:47:47 -0400

    Created a commands folder that holds command objects

---
+++ src/main/java/cohen/recipe/practiceapp/demospring5recipepracticeapp/commands/CategoryCommand.java
@@ -0,0 +1,12 @@
+package cohen.recipe.practiceapp.demospring5recipepracticeapp.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CategoryCommand {
+    
+}
